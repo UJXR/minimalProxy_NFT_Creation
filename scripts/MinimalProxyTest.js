bd720ca0b6cad0a4364709f12c6810004257e5d7
ujjwal <ujjwal@intract.buzz>
2023-03-01 11:49:45 +0530

    Simple example of minimal proxy is complete

---
+++ scripts/MinimalProxyTest.js
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+
+async function main() {
+
+    // load the clone
+    const proxy = await hre.ethers.getContractAt(
+        "NFTImplementationContract",
+        "0x56650E32f2E35C15EdBd9C0915557e7eE5b8cA1e"
+    );
+
+    console.log("Proxy is initialized == ", await proxy.isInitialized()); // get initialized boolean == true
+    console.log("Proxy name is == ", await proxy.name()); // get initialized boolean == true
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
